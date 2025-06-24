13778b76d94d11ba99c143a179620083f64b7a5c
forjbj <forjbj@gmail.com>
2025-06-24 14:30:29 +1000

    fix intersectionObserver bug in safari

--- src/app/display-book/display-book.component.ts
+++ src/app/display-book/display-book.component.ts
@@ -148,7 +148,7 @@ export class DisplayBookComponent implements AfterViewInit, OnDestroy {
     const options = {
       root: null, // viewport
       threshold: [0],
-      rootMargin: "-1% 0px -97% 0px", //only top verse/s Don't change these affects reloading at correct verse
+      rootMargin: "-3% 0px -95% 0px", //only top verse/s Don't change these affects reloading at correct verse
       delay: 700, //only works on safari
     };
     this.observer = new IntersectionObserver((entries) => {
